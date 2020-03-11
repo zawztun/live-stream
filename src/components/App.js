9ba30f0d76e62775a6acb4ff4e76c35fd150770a
Zaw Zaw Tun <zawzawtun@Zaw-Tun.local>
2020-03-11 02:32:35 -0400

    test

---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+import StreamList from './streams/StreamList';
+import StreamCreate from './streams/StreamCreate';
+import StreamShow from './streams/StreamShow';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import Header from './Header';
+
+
+
+
+
+const App = () => {
+    return (
+    <div>
+        <BrowserRouter>
+        <Header />
+        <div>
+           <Route path = '/' exact component={StreamList} />
+           <Route path = '/streams/new' component={StreamCreate} />
+           <Route path = '/streams/show' component={StreamShow} />
+           <Route path = '/streams/edit' component={StreamEdit} />
+           <Route path = '/streams/delete' component={StreamDelete} />
+        </div>
+        </BrowserRouter>
+    </div>
+      
+    
+    )
+}
+
+export default App;
---
+++ src/components/streams/StreamCreate.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Field, reduxForm}from 'redux-form';
+
+class StreamCreate extends React.Component {
+    renderInput ({input}){
+      
+        return <input {...input} />
+    }
+
+    render(){
+    return (
+        <form>
+            <Field name = "title" component = {this.renderInput}/>
+            <Field name = "description" component = {this.renderInput}/>
+        </form>
+    )
+}
+}
+export default reduxForm({
+    form:'streamCreate'
+})(StreamCreate);
